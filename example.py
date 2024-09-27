4adfbb5a86853f2d0c198ca31b1d988047792895
Mohamed Fazil <mohamedfazilsulaiman@gmail.com>
2024-09-27 16:49:55 -0700

    fix example

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 #@title Configuration:
 #@markdown Choose image paths:
 import cv2
+from extractor import ViTExtractor
 
 image_path1 = 'images/cat.jpg' #@param
 image_path2 = 'images/ibex.jpg' #@param
@@ -31,8 +32,8 @@ import torch
 from correspondences import find_correspondences, draw_correspondences
 
 with torch.no_grad():
-    points1, points2, image1_pil, image2_pil = find_correspondences(image1, image2, num_pairs, load_size, layer,
-                                                                   facet, bin, thresh, model_type, stride)
+    points1, points2, image1_pil, image2_pil = find_correspondences(ViTExtractor(model_type,stride), image1, image2, num_pairs, load_size, layer,
+                                                                   facet, bin, thresh)
 fig_1, ax1 = plt.subplots()
 ax1.axis('off')
 ax1.imshow(image1_pil)
